5689a3b40ee89c78669d815f0f80ecb6fe16f544
shhdh <114096024+razzikhawaja@users.noreply.github.com>
2022-11-06 19:56:27 +0500

    Update House.js

--- backend/models/House.js
+++ backend/models/House.js
@@ -6,6 +6,7 @@ const House = new Schema({
     hNo : { type : String, required : true, max : [127, "Max Length is 127 characters"] },
     blockN : { type : String, required : true, max : [127, "Max Length is 127 characters"] },
     desc : { type : String, required : true, max : [127, "Max Length is 127 characters"] },
+    Status : { type : String, required : true, max : [12, "Max Length is 12 characters"] },
     type : { type : String, required : true, max : [127, "Max Length is 127 characters"] },
     ownerId : { type : String, required : true, max : [127, "Max Length is 127 characters"] },
     residentId : { type : String, required : true, max : [127, "Max Length is 127 characters"] },
